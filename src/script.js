98ee4d3f785c2aecad790844ed5637719bf1655c
HongSenDu <hongsendu@gmail.com>
2020-04-28 18:19:16 -0400

    added basic structure

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
 import enterView from 'enter-view';
 import textBalancer from 'text-balancer';
 import './scripts/page';
-
+import $ from 'jquery';
+import {selectAll} from 'd3-selection';
 import { spectate as spectateConfig } from '../package.json';
 const { USE_NEWS_NAV, USE_EYE_NAV, USE_COVER_HED } = spectateConfig;
 
@@ -35,4 +36,29 @@ export function hamburgerTrigger() {
 
 if (window.innerWidth <= 460) {
   textBalancer.balanceText('.headline, .deck, .image-caption-text');
+}
+
+$( document ).ready(function() {
+  selectAll('.section').style('height', window.innerHeight + 100);
+});
+
+var rtime;
+var timeout = false;
+var delta = 200;
+$(window).resize(function() {
+    rtime = new Date();
+    if (timeout === false) {
+        timeout = true;
+        setTimeout(resizeend, delta);
+    }
+});
+
+
+function resizeend() {
+    if (new Date() - rtime < delta) {
+        setTimeout(resizeend, delta);
+    } else {
+        timeout = false;
+        selectAll('.section').style('height', window.innerHeight + 100);
+    }               
 }
